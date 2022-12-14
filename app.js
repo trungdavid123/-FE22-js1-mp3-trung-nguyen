93fa72f4470e4ef9a90bf15a346ba82a01b07e05
trung <trungdavid969@gmail.com>
2022-12-14 07:12:54 +0100

    fixed errors

--- app.js
+++ app.js
@@ -16,32 +16,34 @@ const submit = (e) => {
             .then((data) => {
                 if (data.status === 404) {
                     alert("Not Found");
+                } else {
+                    data.sort((a, b) => (a.population < b.population ? 1 : -1));
+                    data.forEach((country) => {
+                        const name = document.createElement("h2");
+                        (li = document.createElement("li")),
+                            (subregion = document.createElement("p")),
+                            (capital = document.createElement("p")),
+                            (population = document.createElement("p")),
+                            (flag = document.createElement("img"));
+                        if (data[0].name === country.name) {
+                            li.classList.add("marked");
+                        }
+
+                        name.innerText = `Country: ${country.name.common}`;
+                        capital.innerText = `Capital: ${
+                            country.capital ? country.capital : "No Capital"
+                        }`;
+                        subregion.innerText = `Subregion: ${country.subregion}`;
+                        population.innerText = `Population: ${country.population} people`;
+                        flag.src = country.flags.png;
+
+                        li.append(name, capital, subregion, population, flag);
+                        list.append(li);
+                    });
                 }
-                data.sort((a, b) => (a.population < b.population ? 1 : -1));
-                data.forEach((country) => {
-                    const name = document.createElement("h2");
-                    (li = document.createElement("li")),
-                        (subregion = document.createElement("p")),
-                        (capital = document.createElement("p")),
-                        (population = document.createElement("p")),
-                        (flag = document.createElement("img"));
-                    if (data[0].name === country.name) {
-                        li.classList.add("marked");
-                    }
-
-                    name.innerText = `Country: ${country.name.common}`;
-                    capital.innerText = `Capital: ${
-                        country.capital ? country.capital : "No Capital"
-                    }`;
-                    subregion.innerText = `Subregion: ${country.subregion}`;
-                    population.innerText = `Population: ${country.population} people`;
-                    flag.src = country.flags.png;
-
-                    li.append(name, capital, subregion, population, flag);
-                    list.append(li);
-                });
             })
             .catch((err) => alert(err));
+    } else {
     }
 
     list.innerHTML = "";
